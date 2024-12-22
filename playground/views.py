3c26ebc64a73680aae4419205a290e9926afd63a
14elias <eliasmebrahtom1994@gmail.com>
2024-12-22 12:48:16 -0800

    customizing admin

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.db.models.aggregates import Max,Min,Avg,Count
 from django.http import HttpResponse
-
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product,OrderItem,Order
+from tags.models import *
 def hello(request):
-    return render(request,'playground/hello.html', {'name':'elias'})
+    queryset=TaggedItem.objects.get_tags_for(Product,1)
+    return render(request,'playground/hello.html', {'result':list(queryset)})
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['title','unit_price','inventory_status']
+    list_per_page=10
+    @admin.display(ordering='inventory')
+    def inventory_status(self,product):
+        if product.inventory<10:
+            return 'low'
+        return 'ok'
+
+admin.site.register(Collection)
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=['first_name','last_name','membership']
+    ordering=['first_name','last_name']
+    list_editable=['membership']
+    list_per_page=10
